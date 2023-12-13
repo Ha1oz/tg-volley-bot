2320ce422448cededa65cdb56ad30cc544469fa0
Ha1oz <oleg.barkalov@gmail.com>
2023-12-13 20:09:49 +0300

    feat: first steps

---
+++ volleybot/src/main/java/com/haloz/volleybot/VolleybotApplication.java
@@ -0,0 +1,13 @@
+package com.haloz.volleybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleybotApplication.class, args);
+	}
+
+}
---
+++ volleybot/src/test/java/com/haloz/volleybot/VolleybotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haloz.volleybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
